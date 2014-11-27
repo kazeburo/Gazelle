a535b0fedb5bc4ba383c450647b92e2a70776920
Masahiro Nagano <kazeburo@gmail.com>
2014-11-27 12:57:31 +0900

    Checking in changes prior to tagging of version 0.07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d59462d..031f840 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl extension Gazelle
    
     {{$NEXT}}
    
    +0.07 2014-11-27T03:56:03Z
    +
    +    - has compatibility with HTTP::Parser::XS
    +    - fix failed test in FreeBSD
    +
     0.06 2014-11-18T08:02:38Z
    
         - fix path_info. add 0 terminate

--- lib/Gazelle.pm
+++ lib/Gazelle.pm
@@ -4,7 +4,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 1;
 
--- lib/Plack/Handler/Gazelle.pm
+++ lib/Plack/Handler/Gazelle.pm
@@ -13,7 +13,7 @@ use Server::Starter ();
 use Try::Tiny;
 use Guard;
 
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
