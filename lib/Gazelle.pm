bf669addecb5366cb4a2a952ba1f5549a9e8b603
Masahiro Nagano <kazeburo@gmail.com>
2014-12-25 16:00:02 +0900

    Checking in changes prior to tagging of version 0.11.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 607310c..14c6f85 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Gazelle
    
     {{$NEXT}}
    
    +0.11 2014-12-25T06:58:30Z
    +
    +  - tweek header order
    +
     0.10 2014-12-05T05:45:09Z
    
       - fix test. failed test if there is transparent proxy.

--- lib/Gazelle.pm
+++ lib/Gazelle.pm
@@ -4,7 +4,7 @@ use 5.008001;
 use strict;
 use warnings;
 
-our $VERSION = "0.10";
+our $VERSION = "0.11";
 
 1;
 
--- lib/Plack/Handler/Gazelle.pm
+++ lib/Plack/Handler/Gazelle.pm
@@ -13,7 +13,7 @@ use Server::Starter ();
 use Try::Tiny;
 use Guard;
 
-our $VERSION = "0.10";
+our $VERSION = "0.11";
 
 use XSLoader;
 XSLoader::load(__PACKAGE__, $VERSION);
